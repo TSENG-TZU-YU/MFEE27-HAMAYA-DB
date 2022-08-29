e863c88d715851707d765e01065cf561e321cbf7
Joy <q39078273@gmail.com>
2022-08-29 17:37:28 +0800

    be

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.SERVER_PORT;
+const pool = require('./utils/db.js');
+
+app.get('/api', (req, res, next) => {
+  console.log('這裡是首頁');
+  res.send('Hello Express');
+});
+
+app.use((req, res, next) => {
+  console.log('在所有路由中間件的下面 -> 404 了！');
+  res.status(404).send('Not Found!!');
+});
+
+// 啟動 server，並且開始 listen 一個 port
+app.listen(port, () => {
+  console.log(`server start at ${port}`);
+});
---
+++ utils/db.js
@@ -0,0 +1,18 @@
+// 初始化 dotenv
+require('dotenv').config();
+const mysql = require('mysql2');
+// TODO: createPool
+let pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+    // 請保持 date 是 string，不要轉成 js 的 date 物件
+    dateStrings: true,
+  })
+  .promise();
+
+module.exports = pool;
