52c18c6aa709c372fd92f60d4a35a51eba151554
Joy <q39078273@gmail.com>
2022-09-08 11:09:44 +0800

    課程

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.SERVER_PORT;
-const pool = require('./utils/db.js');
+// const pool = require('./utils/db.js');
+
+const cors = require('cors');
+app.use(cors());
+
+let classAdult = require('./routers/class/classAdult');
+app.use(classAdult);
 
 app.get('/api', (req, res, next) => {
-  console.log('這裡是首頁');
-  res.send('Hello Express');
+    console.log('這裡是首頁');
+    res.send('Hello Express');
 });
 
 app.use((req, res, next) => {
-  console.log('在所有路由中間件的下面 -> 404 了！');
-  res.status(404).send('Not Found!!');
+    console.log('在所有路由中間件的下面 -> 404 了！');
+    res.status(404).send('Not Found!!');
 });
 
 // 啟動 server，並且開始 listen 一個 port
 app.listen(port, () => {
-  console.log(`server start at ${port}`);
+    console.log(`server start at ${port}`);
 });
