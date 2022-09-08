b44782a62125569c9c12b437f285e7210852f24d
Steven <a77563166@yahoo.com.tw>
2022-09-08 17:48:08 +0800

    add place routers code

--- routers/place.js
+++ routers/place.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db');
 
-
+router.post('/rent', (req, res, next) => {
+    console.log('123', req.body);
+    // const datetime = req.rent.usedate + req.rent.time
+    // console.log(datetime);
+    // let [data] = await pool.execute(`INSERT INTO venue_reservation `);
+    // console.log('1');
+    res.json({ message: 'ok' });
+});
 
 module.exports = router;
