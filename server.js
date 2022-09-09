76b595a036d92f3686749b6164255c3e71815b46
Emmafan <emmafan1114@gmail.com>
2022-09-09 15:53:08 +0800

    解衝突

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const pool = require('./utils/db.js');
 
 const cors = require('cors');
 app.use(cors());
-<<<<<<< HEAD
-=======
 app.use(express.json());
->>>>>>> b44782a62125569c9c12b437f285e7210852f24d
 
 let authRouter = require('./routers/auth');
 app.use('/api/auth', authRouter);
@@ -32,23 +29,12 @@ app.use('/api/place', placeRouter);
 let aboutusRouter = require('./routers/aboutus');
 app.use('/api/aboutus', aboutusRouter);
 
-<<<<<<< HEAD
-let products = require('./routers/products');
-app.use(products);
-
-app.get('/api', (req, res, next) => {
-  console.log('這裡是首頁');
-  res.send('Hello Express');
-});
-
-=======
->>>>>>> b44782a62125569c9c12b437f285e7210852f24d
 app.use((req, res, next) => {
-  console.log('在所有路由中間件的下面 -> 404 了！');
-  res.status(404).send('Not Found!!');
+    console.log('在所有路由中間件的下面 -> 404 了！');
+    res.status(404).send('Not Found!!');
 });
 
 // 啟動 server，並且開始 listen 一個 port
 app.listen(port, () => {
-  console.log(`server start at ${port}`);
+    console.log(`server start at ${port}`);
 });
