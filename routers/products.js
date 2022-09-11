6ac0198fd03abe861fcc40b248ea3d878ad51ee5
Emmafan <emmafan1114@gmail.com>
2022-09-11 15:58:53 +0800

    products

--- routers/products.js
+++ routers/products.js
@@ -30,12 +30,7 @@ router.get('/', async (req, res) => {
 
     // 商品類別 - 次類別
     let [categorySub] = await pool.execute(
-        'SELECT product_category.ins_main AS mainId, product_ins_sub.name AS subName FROM product_category INNER JOIN product_ins_sub ON product_ins_sub.id = product_category.ins_sub'
-    );
-
-    // 商品類別
-    let [category] = await pool.execute(
-        'SELECT product_category.*, product_ins_main.name AS mainName, product_ins_sub.name AS subName FROM product_category INNER JOIN product_ins_main ON product_ins_main.id = product_category.ins_main INNER JOIN product_ins_sub ON product_ins_sub.id = product_category.ins_sub'
+        'SELECT product_category.ins_main AS mainId, product_ins_sub.id AS subId, product_ins_sub.name AS subName FROM product_category INNER JOIN product_ins_sub ON product_ins_sub.id = product_category.ins_sub'
     );
 
     // 把取得的資料回覆給前端
@@ -47,12 +42,42 @@ router.get('/', async (req, res) => {
             lastPage, // 總頁數
         },
         data,
-        category,
         categoryMain,
         categorySub,
     });
 });
 
-// 取商品種類
+// // GET http://localhost:3000/products?main_id=1&sub_id=1
+router.get('/:mainId&:subId', async (req, res) => {
+    const mainId = req.params.mainId;
+    const subId = req.params.subId;
+
+    // 分頁
+    let page = req.query.page || 1;
+    // 每一頁拿24筆資料
+    const perPage = 24;
+    // 取得總筆數
+    let [total] = await pool.execute('SELECT COUNT(*) AS total FROM product WHERE ins_main_id = ? && ins_sub_id = ? ', [mainId, subId]);
+    total = total[0].total;
+    // 計算總頁數 Math.ceil
+    let lastPage = Math.ceil(total / perPage);
+    // 計算 offset
+    const offset = perPage * (page - 1);
+
+    let [data] = await pool.execute(
+        'SELECT * FROM product JOIN product_img ON product_img.product_id = product.product_id WHERE ins_main_id = ? && ins_sub_id = ? ORDER BY product.create_time LIMIT ? OFFSET ?',
+        [mainId, subId, perPage, offset]
+    );
+    // 把取得的資料回覆給前端
+    res.json({
+        pagination: {
+            total, // 總共有幾筆
+            perPage, // 一頁有幾筆
+            page, // 目前在第幾頁
+            lastPage, // 總頁數
+        },
+        data,
+    });
+});
 
 module.exports = router;
