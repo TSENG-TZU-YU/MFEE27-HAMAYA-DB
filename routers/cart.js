bb82fce684fdb0b63db801939e3ac2225f9f2e72
wen <owilalala974@gmail.com>
2022-09-15 11:40:23 +0800

    [mod] 會員購物車顯示

--- routers/cart.js
+++ routers/cart.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res, next) => {
     // console.log('cart 中間件', req.body);
     const [data] = req.body;
     try {
-        let saveItemData = await pool.execute(`INSERT INTO cart (user_id, product_id, category_id, amount) VALUE (?,?,?,?)`, [
+        let saveItemData = await pool.execute(`INSERT INTO user_cart (user_id, product_id, category_id, amount) VALUE (?,?,?,?)`, [
             data.user_id,
             data.product_id,
             data.category_id,
@@ -22,18 +22,37 @@ router.post('/', async (req, res, next) => {
 });
 
 //http://localhost:3001/api/cart
-//DELETE FROM cart WHERE user_id=2 AND product_id=A123
+//DELETE FROM user_cart WHERE user_id=2 AND product_id=A123
 //會員刪除購物車內容
 router.delete('/', async (req, res, next) => {
-    // console.log('取得該會員要刪除cart 內容', req.body);
+    // console.log('取得該會員要刪除user_cart內容', req.body);
     const user_id = req.body.user_id;
     const product_id = req.body.product_id;
     try {
-        let response = await pool.execute('DELETE FROM cart WHERE (user_id=?) AND (product_id=?);', [user_id, product_id]);
+        let response = await pool.execute('DELETE FROM user_cart WHERE (user_id=?) AND (product_id=?);', [user_id, product_id]);
         // console.log('delete response', response);
         res.json({ message: '已成功刪除購物車內容，可以去會員專區 > 購物車查看，謝謝' });
     } catch (err) {
         console.log({ message: '刪除失敗' });
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    console.log('req.params', req.params);
+    const user_id = req.params.id;
+    try {
+        let [response] = await pool.execute(
+            `SELECT user_cart.*, product.product_id,product.name,product.price,product_img.image FROM (user_cart INNER JOIN product on product.product_id = user_cart.product_id) INNER JOIN product_img on user_cart.product_id = product_img.product_id WHERE user_id= ?`,
+            [user_id]
+        );
+        console.log('get response', response);
+        if (response) {
+            res.json({ user_id: user_id, items_amount: response.length, message: 'GET 購物車 資料成功', myCart: response });
+        } else {
+            res.json({ message: 'NOT GET 購物車資料 可能資料表為空' });
+        }
+    } catch (err) {
+        console.log({ message: '購物車查詢失敗' });
     }
 });
 
