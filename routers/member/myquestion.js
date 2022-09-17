cfbd275bb1c2b8a8d0f57f77a9e16c4b8bbbf847
RueiYuHuang <b98170022@gmail.com>
2022-09-17 14:41:37 +0800

    update myquestion

--- routers/member/myquestion.js
+++ routers/member/myquestion.js
@@ -6,7 +6,10 @@ const pool = require('../../utils/db');
 //http://localhost:3001/api/member/myquestion
 router.get('/loading', async (req, res, next) => {
     console.log('loading myQuestion');
-    let [myQuestion] = await pool.execute('SELECT user_qna.*  FROM user_qna WHERE user_id=? ORDER BY create_time DESC', [req.session.member.id]);
+    let [myQuestion] = await pool.execute(
+        'SELECT user_qna.*, user_q_category.name AS user_q_category  FROM user_qna JOIN user_q_category ON user_qna.q_category = user_q_category.id WHERE user_id=? ORDER BY create_time DESC',
+        [req.session.member.id]
+    );
     res.json(myQuestion);
 });
 
