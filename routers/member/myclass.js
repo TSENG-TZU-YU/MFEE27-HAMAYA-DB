96b1cc38d85bb43f096e42591c58c347fd85b4d3
Joy <q39078273@gmail.com>
2022-09-19 20:39:59 +0800

    評價

--- routers/member/myclass.js
+++ routers/member/myclass.js
@@ -32,10 +32,17 @@ router.post('/', async (req, res, next) => {
 router.get('/:memberId', async (req, res, next) => {
     const memberId = req.params.memberId;
     let [data] = await pool.execute('SELECT * FROM  class_evaluation WHERE member_id=?', [memberId]);
-    console.log(' memberId', memberId);
-    console.log('data', data);
 
-    res.json(data);
+    // 已完成課程
+    let [finishClass] = await pool.execute(
+        `SELECT order_product_detail.* , order_product.user_id ,class.teacher,class.start_date,class.end_date ,class_img.image_1 FROM order_product_detail JOIN order_product ON order_product_detail.order_id=order_product.order_id JOIN class ON order_product_detail.product_id=class.product_id JOIN class_img ON order_product_detail.product_id=class_img.product_id WHERE  order_product_detail.category_id = 'B' && class.end_date<NOW() `
+    );
+    // 開課中
+    let [buyClass] = await pool.execute(
+        `SELECT order_product_detail.* , order_product.user_id ,class.teacher,class.start_date,class.end_date ,class_img.image_1 FROM order_product_detail JOIN order_product ON order_product_detail.order_id=order_product.order_id JOIN class ON order_product_detail.product_id=class.product_id JOIN class_img ON order_product_detail.product_id=class_img.product_id WHERE  order_product_detail.category_id = 'B' && class.end_date>NOW() `
+    );
+    console.log('buyClass', buyClass);
+    res.json({ data, buyClass, finishClass });
 });
 
 module.exports = router;
