8d184b44ef49b767fcc695cf6838faef978a1d23
Lily <lilyhuang841020@gmail.com>
2022-09-19 20:55:00 +0800

    api 串接

--- routers/news.js
+++ routers/news.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db');
 
-//撈出文章的id與種類 GET/news種類
+//撈出文章的id與種類 GET/news種類for前端的NEWs切換頁
+// http://localhost:3001/api/news?categoryId=4
 router.get('/', async (req, res, next) => {
     const categoryId = req.query.categoryId;
     if (categoryId) {
@@ -19,25 +20,16 @@ router.get('/', async (req, res, next) => {
     res.json({ data });
 });
 
-//撈出文章的id與種類 GET/news種類
-// http://localhost:3001/api/news/section?news=1
-// router.get('/section?:category', async (req, res, next) => {
-//     const NewsCategory = req.query.news;
-//     let [data] = await pool.execute(
-//         `SELECT article.id, article.image, article.title, article.creation_date, article_category.id AS categoryId, article_category.name As categoryName FROM article JOIN article_category ON article.category=article_category.id WHERE article.category=? ORDER BY article.creation_date DESC LIMIT 6`,
-//         [NewsCategory]
-//     );
-
-//     // 把取得的資料回覆給前端
-//     res.json(data);
-// });
-
-// //關聯資料表
-// router.get('/news/articles', async (req, res, next) => {
-//     let [NewsArticles] = await pool.execute('SELECT * FROM article');
-//     console.log('result', NewsArticles);
-//     console.log(NewsArticles);
-//     res.json({ NewsArticles });
-// });
+// http://localhost:3001/api/news/section?categoryList=4
+//TODO:要將categoryid與articleid改名
+router.get('/section?:categoryList', async (req, res, next) => {
+    const categoryList = req.query.categoryList;
+    let [data] = await pool.execute(
+        `SELECT article.*,article_category.* FROM article JOIN article_category ON article.category=article_category.id WHERE article.category=? ORDER BY article.creation_date DESC LIMIT 2
+        `,
+        [categoryList]
+    );
+    res.json({ data });
+});
 
 module.exports = router;
