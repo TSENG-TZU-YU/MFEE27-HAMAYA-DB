9d52d143166726a45fa9f709a99f68cda7171dba
HuangRueiYu <b98170022@gmail.com>
2022-09-24 00:55:21 +0800

    add admin login

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -8,20 +8,48 @@ router.use('/customerservice', customerserviceRouter);
 let couponRouter = require('./coupon');
 router.use('/coupon', couponRouter);
 
-// let mybucketlistRouter = require('./mybucketlist');
-// router.use('/mybucketlist', mybucketlistRouter);
+// http://localhost:3001/api//admin/login
+router.post('/login', async (req, res, next) => {
+    console.log('admin login');
+    // console.log('login', req.body);
+    // 確認這個 email 有沒有註冊過
+    let [members] = await pool.execute('SELECT * FROM admin WHERE account = ?', [req.body.account]);
+    if (members.length == 0) {
+        return res.status(401).json({ message: '帳號或密碼錯誤' });
+    }
+    let member = members[0];
+    // //暫時註解
+    // // let compareResult = await bcrypt.compare(req.body.password, member.password);
+    // // if (!compareResult) {
+    // //     // 如果密碼不對，就回覆 401
+    // //     return res.status(401).json({ message: '帳號或密碼錯誤' });
+    // // }
+    //TODO:測試暫時跳過密碼雜湊
+    if (!(req.body.password === member.password)) {
+        // 如果密碼不對，就回覆 401
+        return res.status(401).json({ message: '帳號或密碼錯誤' });
+    }
 
-// let mycartRouter = require('./mycart');
-// router.use('/mycart', mycartRouter);
+    // 密碼比對成功 -> 存在 session
+    let saveMember = {
+        id: member.id,
+        fullName: member.name,
+        account: member.account,
+    };
+    // 把資料寫進 session 裡
+    req.session.member = saveMember;
+    console.log(req.session);
+    // // 回覆前端登入成功
+    res.json(saveMember);
+    // res.json({ message: 'TESTOK' });
+});
 
-// let myorderRouter = require('./myorder');
-// router.use('/myorder', myorderRouter);
-
-
-// let myquestionRouter = require('./myquestion');
-// router.use('/myquestion', myquestionRouter);
-
-// let myplaceRouter = require('./myplace');
-// router.use('/myplace', myplaceRouter);
+//登出
+// http://localhost:3001/api//admin/logout
+router.get('/logout', (req, res, next) => {
+    console.log('admin logout');
+    req.session.member = null;
+    res.json({ message: '已登出' });
+});
 
 module.exports = router;
