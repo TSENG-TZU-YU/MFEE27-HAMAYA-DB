a340b4d7836fe00faf231ad8d5824eeab3e7b8e9
HuangRueiYu <b98170022@gmail.com>
2022-09-26 02:48:18 +0800

    update be order

--- routers/admin/order.js
+++ routers/admin/order.js
@@ -5,9 +5,9 @@ const pool = require('../../utils/db');
 //讀取訂單
 //http://localhost:3001/api/admin/order/loading
 router.get('/loading', async (req, res, next) => {
-    console.log('loading coupon');
+    console.log('loading order');
     let [result] = await pool.execute(
-        'SELECT order_product.* , order_shipment.name AS shipment , order_pay_method.name AS pay_method ,order_pay_state.name AS pay_state , order_state.name AS order_state FROM order_product JOIN order_shipment ON order_product.shipment = order_shipment.id JOIN order_pay_method ON order_product.pay_method = order_pay_method.id JOIN order_pay_state ON order_product.pay_state = order_pay_state.id JOIN order_state ON order_product.order_state = order_state.id ORDER BY create_time'
+        'SELECT order_product.* , order_shipment.name AS shipment , order_pay_method.name AS pay_method ,order_pay_state.name AS pay_state , order_state.name AS order_state ,coupon.name AS coupon_name,coupon.sn, coupon.discount FROM order_product JOIN order_shipment ON order_product.shipment = order_shipment.id JOIN order_pay_method ON order_product.pay_method = order_pay_method.id JOIN order_pay_state ON order_product.pay_state = order_pay_state.id JOIN order_state ON order_product.order_state = order_state.id JOIN coupon ON order_product.coupon_id = coupon.id ORDER BY create_time'
     );
     res.json(result);
 });
@@ -15,7 +15,7 @@ router.get('/loading', async (req, res, next) => {
 //訂單詳細
 //http://localhost:3001/api/admin/order/detail?id=${data.id}
 router.get('/detail', async (req, res, next) => {
-    console.log('loading coupon detail');
+    console.log('loading order detail');
     const orid = req.query.orid;
 
     let [myCouponDetailArray] = await pool.execute(
