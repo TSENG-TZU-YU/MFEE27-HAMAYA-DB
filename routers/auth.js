da369be07a6a3d9fe8bcaea2cd32c7e69cf18f94
Steven <a77563166@yahoo.com.tw>
2022-09-27 17:30:57 +0800

    fix 登入密碼驗證

--- routers/auth.js
+++ routers/auth.js
@@ -181,8 +181,8 @@ router.post('/login', async (req, res, next) => {
     // console.log(' member.password =', member.password);
     // console.log(req.body.password === member.password);
     //TODO:測試暫時跳過密碼雜湊
-    if (!(req.body.password === member.password)) {
-        // 如果密碼不對，就回覆 401
+    let compareResult = await bcrypt.compare(req.body.password, member.password);
+    if (!compareResult) {
         return res.status(401).json({ message: '帳號或密碼錯誤' });
     }
 
